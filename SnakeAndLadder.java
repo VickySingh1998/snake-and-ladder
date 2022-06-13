265256ca16fb7c0f8066338261322ca59a523302
VickySingh1998 <vickysingh8697@gmail.com>
2022-06-13 20:05:36 +0530

    modified: to get a random dice number

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,7 +3,8 @@ package com.bridgelabz;
 public class SnakeAndLadder {
     public static void main(String[] args) {
     int player1= 0;
-    System.out.println("Single Player at start position "+player1);
+    int diceRoll = (int) Math.floor(Math.random()*10) % 6 + 1;
+    System.out.println("Player rolled the die and got "+diceRoll);
 
     }
 }
