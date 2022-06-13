e8599a39d384cc7a5c9cdfb37dc83df516b62dcc
VickySingh1998 <vickysingh8697@gmail.com>
2022-06-13 14:28:12 +0530

    added: snake and ladder position

---
+++ SnakeAndLadder.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+    int start = 0;
+    System.out.println("Single Player at start position 0 " );
+    }
+}
